37e1d18d9c7ec06c55d134a7c544ca252028d8ee
NT-marlowe <n.tezuka196.ninj@gmail.com>
2020-12-14 23:46:58 +0900

    added yamanobori but it's not efficient

--- tsp1.c
+++ tsp1.c
@@ -5,6 +5,9 @@
 #include <assert.h>
 #include <unistd.h>
 #include <errno.h> // strtol のエラー判定用
+#include <time.h>
+
+#define INF 1e9 // 最短距離の解の初期値
 
 // 町の構造体（今回は2次元座標）を定義
 typedef struct
@@ -36,6 +39,11 @@ int min(const int a, const int b) {
   return (a < b) ? a : b;
 }
 
+void swap(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
 // プロトタイプ宣言
 // draw_line: 町の間を線で結ぶ
 // draw_route: routeでの巡回順を元に移動経路を線で結ぶ
@@ -47,7 +55,10 @@ void draw_line(Map map, City a, City b);
 void draw_route(Map map, City *city, int n, const int *route);
 void plot_cities(FILE* fp, Map map, City *city, int n, const int *route);
 double distance(City a, City b);
-Answer solve(const City *city, int n, int *route, int *visited, int visited_number);
+double total_distance(const City *city, int *route, int n);
+void gen_random_permutation(int *pattern, int n);
+Answer yamanobori(const City *city, int *route, int n);
+Answer solve(const City *city, int n);
 Map init_map(const int width, const int height);
 void free_map_dot(Map m);
 City *load_cities(const char* filename,int *n);
@@ -60,6 +71,7 @@ Map init_map(const int width, const int height)
     dot[i] = tmp + i * height;
   return (Map){.width = width, .height = height, .dot = dot};
 }
+
 void free_map_dot(Map m)
 {
   free(m.dot[0]);
@@ -70,7 +82,7 @@ City *load_cities(const char *filename, int *n)
 {
   City *city;
   FILE *fp;
-  if ((fp=fopen(filename,"rb")) == NULL){
+  if ((fp = fopen(filename,"rb")) == NULL){
     fprintf(stderr, "%s: cannot open file.\n",filename);
     exit(1);
   }
@@ -83,8 +95,12 @@ City *load_cities(const char *filename, int *n)
   fclose(fp);
   return city;
 }
+
 int main(int argc, char**argv)
 {
+  srand(time(NULL));
+  
+  
   // const による定数定義
   const int width = 70;
   const int height = 40;
@@ -101,27 +117,37 @@ int main(int argc, char**argv)
 
   City *city = load_cities(argv[1],&n);
   assert( n > 1 && n <= max_cities); // さすがに都市数100は厳しいので
-
   // 町の初期配置を表示
-  plot_cities(fp, map, city, n, NULL);
+  // plot_cities(fp, map, city, n, NULL);
   sleep(1);
 
   // 訪れる順序を記録する配列を設定
   int *route = (int*)calloc(n, sizeof(int));
   // 訪れた町を記録するフラグ
-  int *visited = (int*)calloc(n, sizeof(int));
+  // int *visited = (int*)calloc(n, sizeof(int));
 
-  const Answer d = solve(city,n,route,visited, 0);
-  plot_cities(fp, map, city, n, route);
-  printf("total distance = %f\n", d);
+  Answer ans = (Answer){ .dist = INF, .route = route};
+  Answer tmp;
+  for (int i = 0; i < 10; i++) {// とりあえず初期解5個
+    tmp = solve(city, n);
+    if (ans.dist > tmp.dist) {
+      ans.dist = tmp.dist;
+      memcpy(ans.route, tmp.route, sizeof(int) * n);
+    }
+    free(tmp.route);
+  }
+  
+  // const Answer d = solve(city,n,route,visited, 0);
+  // plot_cities(fp, map, city, n, ans.route);
+  printf("total distance = %f\n", ans.dist);
   for (int i = 0 ; i < n ; i++){
-    printf("%d -> ", route[i]);
+    printf("%d -> ", ans.route[i]);
   }
   printf("0\n");
 
   // 動的確保した環境ではfreeをする
   free(route);
-  free(visited);
+  // free(visited);
   free(city);
   
   return 0;
@@ -185,7 +211,105 @@ double distance(City a, City b)
   return sqrt(dx * dx + dy * dy);
 }
 
-Answer solve(const City *city, int n, int *route, int *visited, int visited_number)
+void gen_random_permutation(int *pattern, int n) {
+  // srand(time(NULL));
+  pattern[0] = 0;
+
+  int num = n-1;
+  int stock[num];
+  for (int i = 0; i < num; i++) {
+    stock[i] = i+1;
+  }
+  
+  for (int i = 0; i < num; i++) {
+    int idx = rand() % (num-i);
+    pattern[i+1] = stock[idx];
+
+    int tmp = stock[idx];
+    stock[idx] = stock[num-i-1];
+    stock[num-i-1] = tmp;
+  }
+}
+
+double total_distance(const City *city, int *route, int n) {
+  double sum = 0;
+  for (int i = 0; i < n; i++) {
+    const int c0 = route[i];
+    const int c1 = route[(i+1)%n];
+    sum += distance(city[c0], city[c1]);
+  }
+  return sum;
+}
+
+Answer yamanobori(const City *city, int *route, int n) {
+  
+  // 渡された配列の近傍を取る前に、距離を計算しておく
+  int *arg = (int*)calloc(n, sizeof(int));
+  memcpy(arg, route, sizeof(int) * n);
+  Answer origin = (Answer){ .dist = 0, .route = arg};
+  origin.dist = total_distance(city, route, n);
+
+  int hamming2_route[n];
+  // memcpy(hamming2_route, route, sizeof(int) * n);
+  for (int i = 0; i < n; i++) hamming2_route[i] = route[i]; 
+  
+  // ハミング距離2のルートの距離を計算し、短いものがあれば更新する
+  for (int i = 1; i < n-1; i++) {
+    for (int j = i+1; j < n; j++) {
+      // swap(&hamming2_route[i], &hamming2_route[j]);
+      int now = hamming2_route[i];
+      hamming2_route[i] = hamming2_route[j];
+      hamming2_route[j] = now;
+      
+      double tmp = total_distance(city, hamming2_route, n);
+      if (origin.dist > tmp) {
+        origin.dist = tmp;
+        memcpy(origin.route, hamming2_route, sizeof(int) * n);
+      }
+      // swap(&hamming2_route[i], &hamming2_route[j]);
+      now = hamming2_route[i];
+      hamming2_route[i] = hamming2_route[j];
+      hamming2_route[j] = now;
+    }
+  }
+  return origin;
+}
+
+Answer solve(const City *city, int n) { //山登り法としてはsolveだが、最適解を求めてはいない
+  
+  int *route = (int*)calloc(n, sizeof(int)); //137行目でfreeしている
+  
+  gen_random_permutation(route, n); //初期解をセット
+  for (int i = 0; i < n; i++) {
+    printf("%d ", route[i]);
+  }
+  printf("\n");
+  
+  double origin_distance = total_distance(city, route, n);
+
+  
+  // for (int i = 0; i < n; i++) {
+  //   printf("%d ", route[i]);
+  // }
+  // printf("\n");
+
+  Answer ans = (Answer){ .dist = INF, .route = NULL};
+  while (1) {
+    ans = yamanobori(city, route, n);
+    
+    if (ans.dist == origin_distance) {
+      return ans;  //解が更新されなくなったら終了
+    } 
+    else {
+      origin_distance = ans.dist;
+      free(ans.route);
+    }
+  }
+  printf("solve done\n");
+  return ans; 
+}
+
+/*Answer solve(const City *city, int n, int *route, int *visited, int visited_number)
 {
   // 以下はとりあえずダミー。ここに探索プログラムを実装する
   // 現状は町の番号順のルートを回っているだけ
@@ -214,5 +338,4 @@ Answer solve(const City *city, int n, int *route, int *visited, int visited_numb
     const int c1 = route[(i+1)%n]; // nは0に戻る
     sum_d += distance(city[c0],city[c1]);
   }*/
-  return sum_d;
-}
+  
